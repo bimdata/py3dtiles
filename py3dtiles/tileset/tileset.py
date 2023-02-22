5c550f550cd013f3bac50038168ad51f5763ca66
Antoine Facchini <antoine.facchini@oslandia.com>
2023-02-22 10:20:03 +0100

    feat(tileset.py): sync root uri of tiles when the tileset is written as json

--- py3dtiles/tileset/tileset.py
+++ py3dtiles/tileset/tileset.py
@@ -118,25 +118,25 @@ class TileSet(RootProperty[TilesetDictType]):
         # Tile's content (set as their respective TileContent uri) must be
         # specified:
         all_tiles = self.root_tile.get_all_children()
+        all_tiles.append(self.root_tile)
         for index, tile in enumerate(all_tiles):
             tile.content_uri = Path("tiles") / f"{index}.b3dm"
 
         # Proceed with the writing of the TileSet per se:
-        self.write_as_json(target_dir)
+        self.write_as_json(target_dir / "tileset.json")
 
         # Terminate with the writing of the tiles content:
         for tile in all_tiles:
             tile.write_content(directory)
 
-    def write_as_json(self, directory: Path) -> None:
+    def write_as_json(self, tileset_path: Path) -> None:
         """
         Write the tileset as a JSON file.
-        :param directory: the target directory name
+        :param tileset_path: the path where the tileset will be written
         """
         # Make sure the TileSet is aligned with its children Tiles.
         self.root_tile.sync_bounding_volume_with_children()
 
-        tileset_path = directory / "tileset.json"
         with tileset_path.open("w") as f:
             f.write(self.to_json())
 
