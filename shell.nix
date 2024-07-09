61c89ec6f7d1f369933d2dba9d2ebd3f3f2676e3
Augustin Trancart <augustin.trancart@oslandia.com>
2024-07-09 14:35:13 +0200

    fix(nix): don't update pip at each shell invocation

--- shell.nix
+++ shell.nix
@@ -20,9 +20,8 @@ pkgs.mkShell rec {
 
     export TMPDIR=/tmp/pipcache
 
-    python -m pip install --cache-dir=$TMPDIR --upgrade pip
-
-    if ! py3dtiles --help > /dev/null 2>&1; then
+    if [ ! -x .venv/bin/py3dtiles ] > /dev/null 2>&1; then
+      python -m pip install --cache-dir=$TMPDIR --upgrade pip
       python -m pip install --cache-dir="$TMPDIR" -e .\[all,doc,dev,laz\]
       # keep this line after so that ipython deps doesn't conflict with other deps
       python -m pip install ipython debugpy
