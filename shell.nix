e0ec4f35f3abbeb4afde88affb1339f98e8c88f7
Augustin Trancart <augustin.trancart@oslandia.com>
2024-07-16 19:07:55 +0200

    fix(shell.nix): sync with pyproject.toml

--- shell.nix
+++ shell.nix
@@ -22,7 +22,7 @@ pkgs.mkShell rec {
 
     if [ ! -x .venv/bin/py3dtiles ] > /dev/null 2>&1; then
       python -m pip install --cache-dir=$TMPDIR --upgrade pip
-      python -m pip install --cache-dir="$TMPDIR" -e .\[all,doc,dev,laz\]
+      python -m pip install --cache-dir="$TMPDIR" -e .\[postgres,las,ply,dev,doc,pack\]
       # keep this line after so that ipython deps doesn't conflict with other deps
       python -m pip install ipython debugpy
     fi
